86213572023460e5f8c2e68f8ae2f234028598d2
ddobervich <david.dobervich@gmail.com>
2022-01-30 11:42:02 -0800

    update comments

--- src/Interfaces/Drawable.java
+++ src/Interfaces/Drawable.java
@@ -8,7 +8,8 @@ public interface Drawable {
      * A method to allow you to draw on top of the filtered image in the display.
      * This method is run after displaying the filtered image.  You can use
      * processing commands such as rect(...), line(...), ellipse(...) etc. to draw.
-     * ***DO NOT*** do any image processing in this method.  It is for drawing only.
+     * (0, 0) is located at the upper left of the image (not the display window).
+     * Lower right would be (original.getWidth(), original.getHeight()).
      * @param window the PApplet window your filtered image is displayed in
      * @param original the original unfiltered image (in case you want to query pixel values)
      * @param filtered the output from your processImage method
