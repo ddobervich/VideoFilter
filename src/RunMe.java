18d5f6a9f26cddd0b0bec73a8b905c8c864acb1b
ArchitWa <98776209+ArchitWa@users.noreply.github.com>
2024-01-10 14:53:07 -0800

    Update RunMe.java

--- src/RunMe.java
+++ src/RunMe.java
@@ -3,7 +3,7 @@ import core.DisplayWindow;
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("images/6.jpg", 800, 600, "TileFilter");
+        DisplayWindow.showFor("images/6.jpg", 800, 600, "DoNothingFilter");
 
         // --== Determine your input interactively with menus ==--
 //        DisplayWindow.getInputInteractively(800,600);
